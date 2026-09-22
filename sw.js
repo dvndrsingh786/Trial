cec09ca2149a8684e5f951e9b4acb915155a4f33
dvndrsingh786 <dvndrsingh786@gmail.com>
2026-09-22 09:14:33 +0100

    Refresh offline cache for expected progress bar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="japji-paath-v3";
+const CACHE="japji-paath-v4";
 const ASSETS=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
